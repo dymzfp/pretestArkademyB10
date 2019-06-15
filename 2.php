e4c675199eaf6348d6be42730d190d4f99dfaed2
Dimas Febri Prasetyo <dfebri446@gmail.com>
2019-06-16 05:43:46 +0700

    final

--- 2.php
+++ 2.php
@@ -20,48 +20,57 @@
 
 
 	if (isset($_POST['kirim'])) {
-	 	
+
 		$panjang = $_POST['panjang'];
 
-		$prima = array();
+		if ($panjang > 0 AND $panjang <= 10) {
 
-		$i = 2;
-		do {
-			
-			$p = 0; // total pembagi
+			$prima = array();
 
-			// mencari pembagi
-			for($j=1; $j<=$i; $j++){
-				if ($i % $j == 0) {
-					$p += 1;
+			$i = 2;
+			do {
+				
+				$p = 0; // total pembagi
+
+				// mencari pembagi
+				for($j=1; $j<=$i; $j++){
+					if ($i % $j == 0) {
+						$p += 1;
+					}
 				}
+
+				// jika pembagi kurang dari samadengan 2 berarti bilangan prima, masukan ke dalam array
+				if ($p <= 2) {
+					array_push($prima, $i);
+				}
+
+				$i++;
+
 			}
+			while ( count($prima) < $panjang );
 
-			// jika pembagi kurang dari samadengan 2 berarti bilangan prima, masukan ke dalam array
-			if ($p <= 2) {
-				array_push($prima, $i);
+			// var_dump($prima);
+
+			echo "<br><br>";
+			echo "Alas/tinggi = " . $panjang;
+			echo "<br><br>";
+
+			for ($i=0; $i < $panjang; $i++) {
+				
+				// membuat sama dengan pertama, selalu bertambah satu
+				for ($y=0; $y <= $i; $y++) { 
+					echo $prima[$y] . " ";
+				}
+
+				echo "<br>";
 			}
-
-			$i++;
-
+		
 		}
-		while ( count($prima) < $panjang );
-
-		// var_dump($prima);
-
-		echo "<br><br>";
-		echo "Alas/tinggi = " . $panjang;
-		echo "<br><br>";
-
-		for ($i=0; $i < $panjang; $i++) {
-			
-			// membuat sama dengan pertama, selalu bertambah satu
-			for ($y=0; $y <= $i; $y++) { 
-				echo $prima[$y] . " ";
-			}
-
-			echo "<br>";
+		else {
+			echo "masukan alas/tinggi dengan ketentuan : 0 < alas/tinggi <= 10 ";
 		}
+	 	
+		
 
 	} 
 
--- 3.php
+++ 3.php
@@ -22,8 +22,12 @@
 	function pohon($awal, $tahun) {
 
 		$pohon = $awal;
-		for ($i=0; $i < $tahun; $i++) { 
-			$pohon = ($pohon*2)+1;
+		for ($i=0; $i < $tahun; $i++) {
+			// saat musim semi 
+			$pohon = $pohon+($pohon*2);
+		
+			// saat musim gugur
+			$pohon += 1;
 		}
 
 		echo "tinggi akhir pohon = {$pohon} m";
@@ -34,7 +38,13 @@
 		$x = $_POST['awal'];
 		$y = $_POST['tahun'];
 
-		pohon($x, $y);
+		if ($x > 0 AND $y > 0) {
+			pohon($x, $y);
+		}
+		else {
+			echo "Masukan tinggi awal dan tahun > 0";
+		}
+
 	}
 
 ?>
